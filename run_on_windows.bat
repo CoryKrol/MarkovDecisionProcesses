fa772743494658a34bcebdaf1a2f46af96fd77bd
coryt <coryteee@gmail.com>
2017-10-15 20:25:22 -0500

    Fixed Windows batch script

--- run_on_windows.bat
+++ run_on_windows.bat
@@ -7,7 +7,7 @@ mkdir out\production
 mkdir out\production\MarkovDecisionProcesses\
 javac @sources.txt -d out\production\MarkovDecisionProcesses\
 
-java -cp out\production\MarkovDecisionProcesses\ com.ctk150230.markov.Main %1 %2
+java -cp out\production\MarkovDecisionProcesses\ com.ctk150230.markov.Main %1 %2 %3 %4
 
 :error
 @echo Error: Not enough arguments supplied, please provide <number_of_states_in_MDP> <num_of_possible_actions> <input_filename> <discount_factor_𝛄>
